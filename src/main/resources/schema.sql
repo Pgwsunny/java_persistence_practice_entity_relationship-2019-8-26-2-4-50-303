77ffa22bd51f1e56d0e96404dbfcd77d4741f87b
Pgwsunny <814424014@qq.com>
2019-08-29 20:43:37 +0800

    sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,24 @@
-CREATE TABLE employee (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(64) NOT NULL,
-  age   int(4) NOT NULL
+
+CREATE TABLE if not exists systemadministrator (
+  sysadmid INTEGER PRIMARY KEY,
+  username VARCHAR(64) NOT NULL,
+  password   VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE if not exists company (
+  companyid INTEGER PRIMARY KEY,
+  companyadminid INTEGER NOT NULL,
+  companyname   VARCHAR(64) NOT NULL,
+  sysadmid   INTEGER FOREIGN KEY NOT NULL
+);
+
+CREATE TABLE if not exists employee (
+  employeeid INTEGER PRIMARY KEY,
+  companyid INTEGER FOREIGN KEY NOT NULL
+);
+
+CREATE TABLE if not exists companyadministrator (
+  companyadminid INTEGER NOT NULL,
+  companyadminname  VARCHAR(64) NOT NULL,
+  employeeid FOREIGN KEY NOT NULL
 );
